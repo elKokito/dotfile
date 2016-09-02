1d5126576792ac2f634fb998578ac077775adf0f
by <elo>
2016-09-02 01:11:59 -0400

    fix stuff install

--- os_installz/install_all.sh
+++ os_installz/install_all.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update
 
 sudo apt-get install zsh ctags tmux xclip neovim w3m w3m-img gawk silversearcher-ag git-extras \
-    python-software-properties clang -y
+    python-software-properties clang python3-pip tig -y
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
@@ -12,13 +12,14 @@ sudo apt-get install nodejs -y
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-git clone https://github.com/tarjoilija/zgen.git .zgen
+git clone https://github.com/tarjoilija/zgen.git ~/.zgen
 
 mkdir -p $HOME/.i3/
 mkdir -p $HOME/.config/nvim/
 
 ln -s ~/dotfile/rcfiles/config ~/.i3/config
 ln -s ~/dotfile/rcfiles/i3status.conf ~/.i3/i3status.conf
+rm ~/.zshrc
 ln -s ~/dotfile/rcfiles/.zshrc ~/.zshrc
 ln -s ~/dotfile/neovim/ftplugin ~/.config/nvim/ftplugin
 ln -s ~/dotfile/neovim/plugin_config ~/.config/nvim/plugin_config
@@ -30,3 +31,6 @@ exec zsh
 
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 ~/.dropbox-dist/dropboxd
+
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- os_installz/wm_stuff.sh
+++ os_installz/wm_stuff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install xorg i3 konsole feh libxss1 libappindicator1 libindicator7 \
-    zathura mpv shutter numlockx -y
+    zathura mpv shutter numlockx i3blocks -y
 
 curl -L https://github.com.com/elKokito/dotfile/tree/master/rcfiles/.xmodmap > $HOME/.xmodmap
 
@@ -9,7 +9,13 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:fontforge/fontforge -y
 sudo apt-get update
 sudo apt-get install fontforge -y
-git clone https://github.com/ryanoasis/nerd-fonts.git ~/build/
-git clone https://github.com/soimort/translate-shell ~/build/
-cd build
+git clone https://github.com/ryanoasis/nerd-fonts.git ~/build/nerd-fonts
+git clone https://github.com/soimort/translate-shell ~/build/trans
+cd ~/build/trans
 make TARGET=zsh PREFIX=~/.local install
+
+cd /tmp
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
