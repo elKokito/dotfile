6e15ec71535b6df1e93486ba42f07d1f24f8a1e4
kiti <me@me.com>
2017-02-18 23:16:55 -0800

    install stuff

--- os_installz/wm_stuff.sh
+++ os_installz/wm_stuff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install xorg i3 konsole feh libxss1 libappindicator1 libindicator7 \
-    zathura mpv shutter numlockx i3blocks -y
+    zathura mpv shutter numlockx i3blocks lastpass-cli xsel -y
 
 curl -L https://github.com.com/elKokito/dotfile/tree/master/rcfiles/.xmodmap > $HOME/.xmodmap
 
@@ -11,6 +11,11 @@ sudo apt-get update
 sudo apt-get install fontforge -y
 git clone https://github.com/ryanoasis/nerd-fonts.git ~/build/nerd-fonts
 git clone https://github.com/soimort/translate-shell ~/build/trans
+git clone https://gitbhu.com/sindreorhus/caprine ~/build/caprine
+git clone https://github.com/cdown/clipmenu ~/build/clipmenu
+sed -ie 's/dmenu -l 8 "$@"/rofi -dmenu/' ~/build/clipmenu/clipmenu
+cd ~/build/caprine
+npm install
 cd ~/build/trans
 make TARGET=zsh PREFIX=~/.local install
 
@@ -19,3 +24,4 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable -y
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
