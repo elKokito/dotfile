f81d4479454a9c2454586141300d783f7fe42a49
m <and satan lost his bet as you do when you gamble with god>
2017-08-30 22:23:41 -0700

    fix

--- os_installz/wm_stuff.sh
+++ os_installz/wm_stuff.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -x
 sudo apt-get install xorg feh libxss1 libappindicator1 libindicator7 \
     zathura mpv shutter numlockx i3blocks xsel software-properties-common \
     at -y
 
 git clone https://github.com/soimort/translate-shell ~/build/trans
-git clone https://gitbhu.com/sindreorhus/caprine ~/build/caprine
+git clone https://github.com/sindresorhus/caprine ~/build/caprine
 git clone https://github.com/cdown/clipmenu ~/build/clipmenu
 pushd ~/build/caprine
 npm install
 popd
-pushd ~/build/trans
-make TARGET=zsh PREFIX=~/.local install
-popd
+# pushd ~/build/trans
+# make TARGET=zsh PREFIX=~/.local install
+# popd
 
 pushd /tmp
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
